fb9025c7882adf01b5ca664d11774a2a50909b67
Sumeet Dagaonkar <sumeetkd@gmail.com>
2022-09-14 15:00:25 +0530

    Major restructuring of the TFIDF function and addition of the simulation evaluator function to separate tfidf and database

--- src/interface/__main__.py
+++ src/interface/__main__.py
@@ -2,9 +2,11 @@ from flask import Flask, render_template, request, Response
 from src.data import metadata_class
 from src.data import pdfparse_class
 from src.semantic.tfidf import tfidf
+import json
 app = Flask(__name__)
 
 pdfdb = pdfparse_class()
+metadb = metadata_class()
 
 @app.route('/search_form')
 def search_form():
@@ -35,20 +37,132 @@ def search_progressbar():
 
 @app.route('/search_progress', methods = ['GET', 'POST'])
 def search_progress():
-       return Response(pdfdb.search(), mimetype='text/event-stream')
+    return Response(pdfdb.search(), mimetype='text/event-stream')
 
 
 @app.route('/search_results',methods = ['POST', 'GET'])
 def search_results():
-       return render_template("search_results.html",results = pdfdb.search_results())
+    return render_template("search_results.html",results = pdfdb.search_results())
 
 
-@app.route('/selected',methods = ['POST', 'GET'])
+class similarlity_evaluation():
+    """
+    Contains the connections between the database and the tfidf batch evaluator class
+
+    Should initialize tfidf to preserve the counts variables
+    TODO: Move to separate class in the semantic folder
+    """
+
+    def __init__(self, paper_list = [], filter_dict = {}):
+        self.paper_list = paper_list
+        self.filter_dict = filter_dict
+        self.evaluator_instances = []
+        # Moving to help solve repeat evaluation issue
+        # self.metadb = metadata_class()
+
+    def paper_list_update(self, paper_list):
+        # Moved here to help with repeat evaluation
+        self.metadb = metadata_class()
+        self.paper_list = paper_list
+        # To prevent adding of instances to the old list
+        self.evaluator_instances = []
+        for paper_id in paper_list:
+            self.evaluator_instances.append(tfidf(
+                                paper_id,
+                                self.metadb.provide_abstract(paper_id))
+                                            )
+
+    def repr_paper_list(self):
+        return self.paper_list
+
+    def filter_dict_update(self, filter_dict):
+        self.filter_dict = filter_dict
+        self.metadb.param_update(self.filter_dict)
+
+    def evaluator(self, batchsize):
+        """
+        TODO: Not sure if the yielding will work
+        """
+        # Define the count which goes from 0 to the number of papers chosen
+        count = -1
+        # Loop through the list of papers
+        for evaluator in self.evaluator_instances:
+            # super().__init__()
+            count += 1
+            # self.set_paper(paper_id, metadb.provide_abstract(paper_id))
+            for state in evaluator.process_data(
+                    self.metadb.filtered_batch_generator(batchsize)):
+                print("State : {}".format(state))
+                print("Total : {}".format(self.metadb.repr_total_count()))
+                stream_dict = {
+                    'bar': count,
+                    'progress': (state /
+                                 (self.metadb.repr_total_count() // batchsize + 1))*100
+                                }
+                print(stream_dict)
+                yield "data:" + json.dumps(stream_dict) + "\n\n"
+
+    def nearestneighbours(self):
+        similar_papers = []
+        for evaluator in self.evaluator_instances:
+            similar_papers += evaluator.tfidf_nn()
+            print(similar_papers)
+        return self.metadb.displayorderedlist(similar_papers)
+        #return self.metadb.displayresults(similar_papers)
+
+
+simeval = similarlity_evaluation()
+
+
+@app.route('/semantic_progressbar', methods=['GET', 'POST'])
+def semantic_progressbar():
+    """
+    Route creates an intermediate page displaying the progress of evaluation for Semantic results.
+    The page '/search_results' provides 'papers_to_compare' and parameters needed to define the subset of papers that will be compared to the papers.
+    """
+    if request.method == 'POST':
+        print(request.form)
+
+        # Passing the list of papers to evaluator
+        papers_to_compare = request.form.getlist('papers')
+        simeval.paper_list_update(papers_to_compare)
+
+        # Parsing paramters to filter database for semantic evaluation and feeding them to evaluator
+        # Might be redundant output and check if result can be directly passed
+        result = request.form.to_dict()
+        # print(result)
+        subject, year_start, year_end = [result['subject']], int(result['year_start']), int(result['year_end'])
+        filter_dict = {
+            'subject': subject,
+            'year_start': year_start,
+            'year_end': year_end
+        }
+        # simeval.filter_dict_update(result)
+        simeval.filter_dict_update(filter_dict)
+
+    # Pass number of papers to build X number of progress bars
+    return render_template(
+                    "semantic_progressbar.html",
+                    num_bars=len(papers_to_compare))
+
+
+@app.route('/semantic_progress', methods=['GET', 'POST'])
+def semantic_progress():
+    return Response(simeval.evaluator(100), mimetype='text/event-stream')
+
+
+@app.route('/semantic_results', methods=['POST', 'GET'])
+def semantic_results():
+    return render_template("semantic_results.html",
+                           results=simeval.nearestneighbours(),
+                           papers_to_compare=simeval.repr_paper_list())
+
+
+@app.route('/selected', methods=['POST', 'GET'])
 def selected():
     semantictfidf = tfidf()
     similar_papers = []
     if request.method == 'POST':
-        #selected_items= request.form.getlist('papers')
         result = request.form.to_dict()
         papers_to_compare = request.form.getlist('papers')
         subject, year_start, year_end = [result['subject']], int(result['year_start']), int(result['year_end'])
@@ -60,7 +174,7 @@ def selected():
             print(similarity_results)
             similar_papers += similarity_results
             print(similar_papers)
-        return render_template("selected.html", results=similar_papers, papers_to_compare=papers_to_compare)
+        return render_template("tfidf_results.html", results=similar_papers, papers_to_compare=papers_to_compare)
 
 
 
--- src/semantic/tfidf.py
+++ src/semantic/tfidf.py
@@ -5,30 +5,54 @@ from src.data import metadata_class
 from scipy.sparse import vstack
 from src.data import dbAccess
 
-class tfidf:
+metadb = metadata_class()
 
-    def __init__(self):
-        self.db = dbAccess()
+class tfidf:
+    """
+    This class evaluates the similarity of a paper with a subset of papers.
+    """
+
+    def __init__(self, paper_id=0, paper_abstract=""):
+        # self.db = dbAccess()
+        self.paper_id = paper_id
+        self.paper_abstract = paper_abstract
+        # Moving counts and full paper ids to where it starts getting edited
+        # self.counts = []
+        # self.full_paper_ids = []
+
+    def set_paper(self, paper_id, abstract):
+        # Might need to be dropped
+        self.paper_id = paper_id
+        self.paper_abstract = abstract
 
     def spacy_tokenizer(self, document):
         nlp = spacy.load("en_core_web_sm")
         tokens = nlp(document)
         tokens = [token.lemma_ for token in tokens if (
-            token.is_stop == False and \
-            token.is_punct == False and \
+            token.is_stop == False and
+            token.is_punct == False and
             token.lemma_.strip()!= '')]
         return tokens
 
     def __generate_vocab(self, paper):
-        metadb = metadata_class()
         data_for_vocab = metadb.provide_abstract(paper)
-        vocab_run = TfidfVectorizer(input = 'content', tokenizer = self.spacy_tokenizer)
+        vocab_run = TfidfVectorizer(input='content', tokenizer = self.spacy_tokenizer)
         #print(data_for_vocab)
         vocab_run.fit_transform([data_for_vocab])
         vocab = vocab_run.vocabulary_
         return vocab
 
-    def CountMatrix(self,abstracts):
+    def generate_vocab(self):
+        vocab_run = TfidfVectorizer(
+            input='content',
+            tokenizer=self.spacy_tokenizer
+        )
+        # print(data_for_vocab)
+        vocab_run.fit_transform([self.paper_abstract])
+        vocab = vocab_run.vocabulary_
+        return vocab
+
+    def CountMatrix(self, abstracts):
         """
         :param abstracts: Abstracts are supplied in the form of a generator
         :return:
@@ -44,6 +68,58 @@ class tfidf:
 #                matrix = vstack(counts)
         return matrix
 
+    def count_vectorizer(self):
+        # Check if this works out
+        return CountVectorizer(
+                                    input='content',
+                                    tokenizer=self.spacy_tokenizer,
+                                    vocabulary=self.generate_vocab()
+                                        )
+
+    def process_data(self, batch_generator):
+        self.counts = []
+        self.full_paper_ids = []
+        batch_number = 0
+        for abstracts, paper_ids in batch_generator:
+            print(type(batch_generator))
+            batch_number += 1
+            count_vectorizer = self.count_vectorizer()
+            self.counts.append(count_vectorizer.fit_transform(abstracts))
+            # print(paper_ids)
+            self.full_paper_ids += paper_ids
+            print('End of Batch')
+            print('Batch Number: {}'.format(batch_number))
+            print('Papers', paper_ids)
+            yield batch_number
+
+
+    def tfidf_nn(self):
+        # Combine the count matrices of the full database
+        fullcount = vstack([item for item in self.counts])
+
+        # Adding the paper that was compared into the full list. Ensures that the paper that is closest is always the paper itself.
+        if self.paper_id not in self.full_paper_ids:
+            abstractcounts = self.count_vectorizer.fit_transform([self.paper_abstract])
+            fullcount = vstack([fullcount, abstractcounts])
+            self.full_paper_ids.append(self.paper_id)
+
+        # Convert the count matrix into a TFIDF matrix
+        transform = TfidfTransformer()
+        tfidfmatrix = transform.fit_transform(fullcount)
+
+        # Use NearestNeighbors from scikit NearestNeighbors to get the indices of the nearest neighbours
+        nbrs = NearestNeighbors(n_neighbors=4, metric='cosine').fit(tfidfmatrix)
+        _, indices = nbrs.kneighbors(tfidfmatrix)
+
+        # NearestNeighbors evaluated for all entries. Get the row relevant to our paper
+        # From the full_paper_ids get the index for the chosen paper
+        # From the row, convert the indices given by NN to actual paper_ids
+        base_paper_index = self.full_paper_ids.index(self.paper_id)
+        nearestpapers = [self.full_paper_ids[i] for i in indices[base_paper_index]]
+        print(nearestpapers)
+        return nearestpapers
+
+    
     def similarity(self, paper_id, findterms):
 
         batchsize = 100
@@ -51,24 +127,26 @@ class tfidf:
         counts = []
 
         vocabulary = self.__generate_vocab(paper_id)
-        count_vector = CountVectorizer(input='content', tokenizer= self.spacy_tokenizer, vocabulary=vocabulary)
+        count_vector = CountVectorizer(input='content', tokenizer=self.spacy_tokenizer, vocabulary=vocabulary)
 
         metadb = metadata_class()
-        for abstracts , paper_ids in metadb.batch_generator(findterms, batchsize):
+        for abstracts, paper_ids in metadb.batch_generator(findterms, batchsize):
             counts.append(count_vector.fit_transform(abstracts))
-            #print(paper_ids)
+            # print(paper_ids)
             full_paper_ids += paper_ids
             print('End of Batch')
 
         fullcount = vstack([item for item in counts])
+        # Check if this is needed after restructuring
         del counts
+
+        # Adding the paper that was compared into the full list. Ensures that the paper that is closest is always the paper itself.
 
         if paper_id not in full_paper_ids:
             abstract = metadb.provide_abstract(paper_id)
             abstractcounts = count_vector.fit_transform([abstract])
-            fullcount = vstack([fullcount,abstractcounts])
+            fullcount = vstack([fullcount, abstractcounts])
             full_paper_ids.append(paper_id)
-
 
         transform = TfidfTransformer()
         tfidfmatrix = transform.fit_transform(fullcount)
